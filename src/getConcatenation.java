8791916aee41335429927d192b39e88ad8610b18
Jaydin <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2022-11-22 21:15:18 -0500

    Solved

---
+++ src/getConcatenation.java
@@ -0,0 +1,12 @@
+public class getConcatenation {
+
+    public static int[] getConcatenation(int[] nums){
+        int t[]=new int[nums.length*2];
+        int s=0;
+        for(int i=0; i<nums.length*2; i++){
+            if(s >= nums.length){s = 0;}
+            t[i] = nums[s]; s++;
+        }
+        return t;
+    }
+}
